2316d265aa70a43dd9d20daacfd2554fd87ceee0
Heetpatel22 <heet11539@gmail.com>
2026-06-24 12:24:32 +0530

    Update config.py for Railway database vars

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from urllib.parse import urlparse
 
-mysql_url = os.environ.get("MYSQLURL") or os.environ.get("DATABASE_URL")
+mysql_url = os.environ.get("MYSQL_PUBLIC_URL") or os.environ.get("MYSQLURL") or os.environ.get("DATABASE_URL")
 
 if mysql_url and (mysql_url.startswith("mysql://") or mysql_url.startswith("mysqls://")):
     url = urlparse(mysql_url)
@@ -11,8 +11,9 @@ if mysql_url and (mysql_url.startswith("mysql://") or mysql_url.startswith("mysq
     DB_PASSWORD = url.password
     DB_NAME = url.path.lstrip('/')
 else:
-    DB_HOST = os.environ.get("DB_HOST", "localhost")
-    DB_PORT = int(os.environ.get("DB_PORT", 3306))
-    DB_USER = os.environ.get("DB_USER", "root")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD", "root")
-    DB_NAME = os.environ.get("DB_NAME", "marketguruhp")
+    # First check Railway's standard MYSQL... variables
+    DB_HOST = os.environ.get("MYSQLHOST") or os.environ.get("DB_HOST", "localhost")
+    DB_PORT = int(os.environ.get("MYSQLPORT") or os.environ.get("DB_PORT", 3306))
+    DB_USER = os.environ.get("MYSQLUSER") or os.environ.get("DB_USERNAME") or os.environ.get("DB_USER", "root")
+    DB_PASSWORD = os.environ.get("MYSQLPASSWORD") or os.environ.get("DB_PASSWORD", "root")
+    DB_NAME = os.environ.get("MYSQLDATABASE") or os.environ.get("DB_DATABASE") or os.environ.get("DB_NAME", "marketguruhp")
